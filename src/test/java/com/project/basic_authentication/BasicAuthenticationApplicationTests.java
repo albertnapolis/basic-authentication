66be1cca2c0336312607446e8b7e962109ef5602
albert napolis <albertnapolis014@gmail.com>
2025-01-29 00:50:47 +0800

    init

---
+++ src/test/java/com/project/basic_authentication/BasicAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.basic_authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
