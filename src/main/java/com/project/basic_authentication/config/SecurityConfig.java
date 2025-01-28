8cbe6b4868643bbfbc919ddd10a95ee666e46f5e
albert napolis <albertnapolis014@gmail.com>
2025-01-29 01:27:38 +0800

    feat: add initial security configuration and test database setup

---
+++ src/main/java/com/project/basic_authentication/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.project.basic_authentication.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.io.IOException;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .securityMatcher("/api/v1/**")
+                .authorizeHttpRequests(request -> request
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
+}
